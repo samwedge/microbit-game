197cfb30f99375be4cd94c9344f8290751d1569d
Sam <samwedge@users.noreply.github.com>
2024-10-22 20:00:33 +0100

    Add enemies

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 from microbit import *
+import time
+import random
+
 
 def pixels_to_image(pixels):
     pixels_str = ':'.join(
@@ -10,36 +13,65 @@ def pixels_to_image(pixels):
     return Image(pixels_str)
 
 
-player_pos = 2
+def row_from_pos(pos):
+    return [
+        9 if pos == 0 else 0,
+        9 if pos == 1 else 0,
+        9 if pos == 2 else 0,
+        9 if pos == 3 else 0, 
+        9 if pos == 4 else 0,
+    ]
 
 
-while True:
-
+def update_player_pos(pos):
     left = button_a.get_presses()
     right = button_b.get_presses()
 
     movement = right - left
 
-    player_pos += movement
+    pos += movement
 
-    if player_pos < 0:
-        player_pos = 0
-    if player_pos > 4:
-        player_pos = 4
+    if pos < 0:
+        pos = 0
+    if pos > 4:
+        pos = 4
+   
+    return pos
 
-    player_row = [
-        9 if player_pos == 0 else 0,
-        9 if player_pos == 1 else 0,
-        9 if player_pos == 2 else 0,
-        9 if player_pos == 3 else 0, 
-        9 if player_pos == 4 else 0,
-    ]
+
+
+
+
+
+player_pos = 2
+frame_start = time.ticks_ms()
+frame_duration = 1000
+enemy_row_1 = [0, 0, 0, 0, 0]
+enemy_row_2 = [0, 0, 0, 0, 0]
+enemy_row_3 = [0, 0, 0, 0, 0]
+enemy_row_4 = [0, 0, 0, 0, 0]
+
+
+while True:
+
+    player_pos = update_player_pos(player_pos)
+
+    current_time = time.ticks_ms()
+    if time.ticks_diff(current_time, frame_start) >= frame_duration:
+        frame_start = current_time
+        enemy_pos = random.randint(0, 4)
+        enemy_row_4 = enemy_row_3
+        enemy_row_3 = enemy_row_2
+        enemy_row_2 = enemy_row_1
+        enemy_row_1 = row_from_pos(enemy_pos)
+
+    player_row = row_from_pos(player_pos)
 
     pixels = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
+        enemy_row_1,
+        enemy_row_2,
+        enemy_row_3,
+        enemy_row_4,
         player_row,
     ]
 
